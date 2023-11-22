c3703e334c7d76fee9bf5ca209273910344ab779
victorflt97 <victorflt97@gmail.com>
2023-11-22 17:48:28 -0600

    Se le agrego titulo a la grafica

--- plot.py
+++ plot.py
@@ -15,4 +15,7 @@ ax.plot(x, y, linewidth=2.0)
 ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
        ylim=(0, 8), yticks=np.arange(1, 8))
 
+ax.set_title("grafica basica")
+
 plt.savefig("grafica.png")
+plt.show()
